b4e20e79f29e8884b94027c04ec2ebab7de73464
Tania.Pash <t.pashnyuk@gmail.com>
2019-05-31 17:23:50 +0300

    ref: fix xo

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const createWindow = require('./lib/create-window');
 const listenEvents = require('./lib/listen-events');
 
 const store = new Store();
-const defaultConf = require('./config');
+const defaultConf = require('./config.json');
 
 app.on('ready', () => {
-	store.set('config',defaultConf);
+	store.set('config', defaultConf);
 	createWindow();
 	setupMenus();
 	listenEvents(store.get('config'));
